3f33a50fb4747f0568e070bd96743405133e4c6b
Hector Polanco <hdp2112@columbia.edu>
2019-10-20 16:44:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,14 @@
 # Your Code Here
 array = [1, 2, 3, -9]
 
-def map(array)
-  yield(array)
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 ) # <== Unique work
+    i += 1
+  end
+  return new
 end
 
 map(array){|n| (n * n)}
