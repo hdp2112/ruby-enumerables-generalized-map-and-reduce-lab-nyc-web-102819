2b252fd735c7fbb3b09e76f97e2ed09a67ef839f
Hector Polanco <hdp2112@columbia.edu>
2019-10-20 17:07:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,25 +16,3 @@ def reduce(array, starting_point=0)
   end
   total
 end
-
-def reduce_to_total(array, starting_point=0)
-  total = starting_point
-  array.length.times do |index|
-    total = total + array[index]
-  end
-  return total
-end
-
-def reduce_to_all_true(array)
-  array.length.times do |index|
-    return false if !array[index]
-  end
-  return true
-end
-
-def reduce_to_any_true(array)
-  array.length.times do |index|
-    return true if array[index]
-  end
-  return false
-end
