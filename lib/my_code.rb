b97dec2528ff7629401008bf94a9ac9d366e2777
Hector Polanco <hdp2112@columbia.edu>
2019-10-20 17:19:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,10 +10,5 @@ end
 print map([1, 2, 3, -9]){|n| n * n}
 
 def reduce(array, starting_point=0)
-  total = starting_point
-  array.length.times do |index|
-    total = total + array[index]
-    yield(false if !array[index] or true if array[index])
-  end
-  total
-end
+  if starting_point
+    
