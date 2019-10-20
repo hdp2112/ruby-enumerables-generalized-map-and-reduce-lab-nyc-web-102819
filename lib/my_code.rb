a0fe1021b8c14c4a5d952302a5a00da41a6cb6ec
Hector Polanco <hdp2112@columbia.edu>
2019-10-20 16:58:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,10 +3,8 @@ array = [1, 2, 3, -9]
 
 def map(array)
   new = []
-  i = 0
-  while i < array.length
-    new.push(yield(array[i]))
-    i += 1
+  array.length.times do |index|
+    new << yield(array[index])
   end
   new
 end
